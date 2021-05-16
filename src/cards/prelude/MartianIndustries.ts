a8daee9b578fbed3a7b31a471cf59d7f0b464b3b
Ben <3masn3@gmail.com>
2021-05-16 11:43:17 -0400

    add energy tag to martian industries

--- src/cards/prelude/MartianIndustries.ts
+++ src/cards/prelude/MartianIndustries.ts
@@ -10,7 +10,7 @@ export class MartianIndustries extends PreludeCard implements IProjectCard {
   constructor() {
     super({
       name: CardName.MARTIAN_INDUSTRIES,
-      tags: [Tags.BUILDING],
+      tags: [Tags.BUILDING, Tags.ENERGY],
 
       metadata: {
         cardNumber: 'P18',
