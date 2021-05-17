27061d1244290f2445199daf459e8ef295c394b4
Ben <3masn3@gmail.com>
2021-05-17 12:17:13 -0400

    git add quantum and warp hub

---
+++ src/cards/community/Quantum.ts
@@ -0,0 +1,48 @@
+import {Card} from '../Card';
+import {CorporationCard} from './CorporationCard';
+import {IProjectCard} from '../IProjectCard';
+import {Tags} from '../Tags';
+import {Player} from '../../Player';
+import {OrOptions} from '../../inputs/OrOptions';
+import {SelectOption} from '../../inputs/SelectOption';
+import {Resources} from '../../Resources';
+import {CardName} from '../../CardName';
+import {CardType} from '../CardType';
+import {CardRenderer} from '../render/CardRenderer';
+
+export class Quantum extends Card implements CorporationCard {
+  constructor() {
+    super({
+      cardType: CardType.CORPORATION,
+      name: CardName.QUANTUM,
+      tags: [Tags.SCIENCE, Tags.POWER],
+      startingMegaCredits: 38,
+
+      metadata: {
+        cardNumber: '',
+        description: 'You start with 38 M€.',
+        renderData: CardRenderer.builder((b) => {
+          b.br;
+          b.megacredits(38);
+          b.corpBox('effect', (ce) => {
+            ce.effect('Each time you play a science tag, including these, increase your energy production.', (eb) => {
+              eb.science(1).played.startEffect.production((pb) => pb.energy(1));
+            });
+          });
+        }),
+      },
+    });
+  }
+
+  public onCardPlayed(player: Player, card: IProjectCard) {
+    if (player.corporationCard !== undefined && player.corporationCard.name === this.name) {
+      player.addProduction(Resources.ENERGY, card.tags.filter((tag) => tag === Tags.SCIENCE).length);
+    }
+    return undefined;
+  }
+
+  public play(player: Player) {
+    player.addProduction(Resources.ENERGY, 1);
+    return undefined;
+  }
+}
---
+++ src/cards/community/WarpHub.ts
@@ -0,0 +1,45 @@
+import {Tags} from '../Tags';
+import {Player} from '../../Player';
+import {CorporationCard} from './../corporation/CorporationCard';
+import {IProjectCard} from '../IProjectCard';
+import {Resources} from '../../Resources';
+import {Card} from '../Card';
+import {CardName} from '../../CardName';
+import {CardType} from '../CardType';
+import {CardRenderer} from '../render/CardRenderer';
+import {Units} from '../../Units';
+
+export class WarpHub extends Card implements CorporationCard {
+  constructor() {
+    super({
+      cardType: CardType.CORPORATION,
+      name: CardName.WARP_HUB,
+      tags: [Tags.SCIENCE, Tags.SPACE],
+      startingMegaCredits: 35,
+
+      cardDiscount: {tag: Tags.SPACE, amount: 7,
+      metadata: {
+        cardNumber: '',
+        description: 'You start with 35 M€.',
+        renderData: CardRenderer.builder((b) => {
+          b.br.br;
+          b.megacredits(35);
+          b.corpBox('effect', (ce) => {
+            ce.effect('When you play a space tag, you pay 7 M€ less for it.', (eb) => {
+              eb.building().played.startEffect.megacredits(-3);
+            });
+          });
+        }),
+      },
+    });
+  }
+
+
+  public getCardDiscount(_player: Player, card: IProjectCard) {
+    return card.tags.filter((tag) => tag === Tags.SPACE).length * 7;
+  }
+
+  public play() {
+    return undefined;
+  }
+}
