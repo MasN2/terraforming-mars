9afc17987174d0bb942c00ece731ef0cb9b49117
Ben <3masn3@gmail.com>
2021-05-19 01:44:54 -0400

    Make tritium scale to player count

--- src/cards/community/TritiumInvestments.ts
+++ src/cards/community/TritiumInvestments.ts
@@ -7,6 +7,7 @@ import {CardName} from '../../CardName';
 import {CardType} from '../CardType';
 import {Card} from '../Card';
 import {CardRenderer} from '../render/CardRenderer';
+import {Size} from '../render/Size';
 import {Resources} from '../../Resources';
 
 export class TritiumInvestments extends Card implements IActionCard, CorporationCard, IResourceCard {
@@ -19,10 +20,10 @@ export class TritiumInvestments extends Card implements IActionCard, Corporation
       cardType: CardType.CORPORATION,
       metadata: {
         cardNumber: '',
-        description: 'You start with 31 M€.',
+        description: 'You start with 31 M€ and 1 MC production per OTHER player. Decrease your TR 1 step.',
         renderData: CardRenderer.builder((b) => {
           b.br.br;
-          b.megacredits(31);
+          b.megacredits(31).production((pb) => pb.megacredits(1).slash().delegates(1)).minus().tr(1, Size.SMALL);
           b.corpBox('action', (ce) => {
             ce.action('Increase your MC production by 1 per camp here, THEN add a camp here.', (eb) => {
               eb.empty().startAction.production((pb) => pb.megacredits(1).slash().camps()).camps();
@@ -35,7 +36,9 @@ export class TritiumInvestments extends Card implements IActionCard, Corporation
 
   public resourceCount = 0;
 
-  public play() {
+  public play(player: Player) {
+    player.addProduction(Resources.MEGACREDITS, player.game.getPlayers().length - 1);
+    player.decreaseTerraformRatingSteps(1);
     return undefined;
   }
 
