ade4f88d4b99b433e2e8780677bb577613f973d9
MasN2 <28033394+MasN2@users.noreply.github.com>
2021-08-15 06:04:35 -0400

    Update Donation.ts
    Donation has been consistently disappointing, it's been given an Earth Tag to make up for the removal of business empire.

--- src/cards/prelude/Donation.ts
+++ src/cards/prelude/Donation.ts
@@ -1,3 +1,4 @@
+import {Tags} from '../Tags';
 import {Player} from '../../Player';
 import {PreludeCard} from './PreludeCard';
 import {CardName} from '../../CardName';
@@ -7,6 +8,7 @@ export class Donation extends PreludeCard {
   constructor() {
     super({
       name: CardName.DONATION,
+      tags: [Tags.EARTH],
 
       metadata: {
         cardNumber: 'P08',
