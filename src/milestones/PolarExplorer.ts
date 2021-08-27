b86beade40c8f8cd048622095dac401093b672ae
Ben <3masn3@gmail.com>
2021-08-27 02:33:51 -0400

    Fix polar explorer for mini board

--- src/milestones/PolarExplorer.ts
+++ src/milestones/PolarExplorer.ts
@@ -9,7 +9,7 @@ export class PolarExplorer implements IMilestone {
         .filter((space) => space.player !== undefined &&
         space.player === player &&
         space.tile !== undefined &&
-        space.y >= 7 && space.y <= 8).length;
+        space.y >= 6 && space.y <= 7).length;
     }
     public canClaim(player: Player): boolean {
       return this.getScore(player) >= 3;
